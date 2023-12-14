5b6bb337fe9b9cbaa0c1b2e0b312ecfa207b6646
Kazuma <21kazuma21@gmail.com>
2023-12-15 00:25:56 +0900

    station9　クリア

--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,7 +1,16 @@
 // @ts-check
+import React from "react"
+import { useEffect } from "react"
+import { useState } from "react"
 
 export const DogListContainer = () => {
-  return <></>
+  const [breeds,setBreeds] = useState([])
+  useEffect(() => {
+    fetch ("https://dog.ceo/api/breeds/list/all")
+      .then(response => response.json())
+      .then(dogData => setBreeds(dogData.message))
+      .then(console.log(breeds))
+  }, [])
 }
 
 export default DogListContainer
