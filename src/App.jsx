27797aa85cb153f407c0bbe2309abc9a31034a23
Kazuma <21kazuma21@gmail.com>
2023-12-14 18:23:22 +0900

    station5　クリア

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 // DO NOT DELETE
-
+import * as React from 'react'
 import './App.css'
+import {useState} from 'react'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl,setDogUrl] = useState("https://dog.ceo/dog-api/")
+  const changeDog = () => setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg")
+
   return (
     <div>
       <header>
         <h2>Dogアプリ</h2>
       </header>
       <p>犬の画像を表示するサイトです。</p>
-      <img src="https://dog.ceo/api/breeds/image/random" alt="犬の画像を取得するAPIです。" />
+      <img src={dogUrl} alt="犬の画像を取得するAPIです。" />
+      <button onClick={changeDog}>更新</button>
 
     </div>
-    
-  )
+  );
 }
